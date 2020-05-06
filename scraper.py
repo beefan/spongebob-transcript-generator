07aeb3f9021e1dec213bdea31f184f21e06a3170
Brandon Fannin <bfannin13@gmail.com>
2020-05-06 12:43:31 -0400

    scrapes fan site for all transcript links

--- scraper.py
+++ scraper.py
@@ -1,12 +1,31 @@
 import urllib.request
-import beautifulsoup4
+from bs4 import BeautifulSoup
+import re
 
-## get html of fandom wiki
-fp = urllib.request.urlopen("https://spongebob.fandom.com/wiki/List_of_transcripts")
-mybytes = fp.read()
-mystr = mybytes.decode("utf8")
-fp.close()
+def main():
+  get_episode_transcripts()
 
+def get_episode_transcripts():
+  ## get html of fandom wiki
+  fp = urllib.request.urlopen("https://spongebob.fandom.com/wiki/List_of_transcripts")
+  mybytes = fp.read()
+  transcriptList = mybytes.decode("utf8")
+  fp.close()
 
+  transcriptSoup = BeautifulSoup(transcriptList, "html.parser")
+  transcriptLinks = transcriptSoup.find_all(is_transcript_link)
+  print(transcriptLinks)
 
-print(mystr)
+def is_transcript_link(tag):
+  isLink = tag.has_attr('href') and tag.has_attr('title')
+  if not isLink:
+    return False
+  
+  isTranscript = ('transcript' in tag['title']) and tag.parent.name == 'center'
+  if not isTranscript:
+    return False
+  
+  headerId = tag.parent.parent.parent.parent.previous_sibling.previous_sibling.contents[0]['id']
+  return re.compile('Season_(?!3)').search(headerId)
+
+main()

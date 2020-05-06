4ee701d6c6cf6d7bb1ffc35ea7e94cd8c9ffa861
Brandon Fannin <bfannin13@gmail.com>
2020-05-06 14:46:11 -0400

    scrapes episode links and creates one document with all transcripts

--- scraper.py
+++ scraper.py
@@ -3,18 +3,61 @@ from bs4 import BeautifulSoup
 import re
 
 def main():
-  get_episode_transcripts()
+  print("main initialized. . .")
+  create_document_from_episodes()
 
-def get_episode_transcripts():
-  ## get html of fandom wiki
-  fp = urllib.request.urlopen("https://spongebob.fandom.com/wiki/List_of_transcripts")
+def create_document_from_episodes():
+  print("opening document spongebob-transcript.txt. . .")
+  document = open('spongebob-transcripts.txt', 'w')
+  for ep in get_episodes():
+    document.write(ep.title)
+    document.write('\n=========================================================')
+    document.write(ep.body)
+    document.write('_________________________________________________________\n')
+  
+  print("closing document. . .")
+  document.close()
+  
+def get_episodes():
+  episodes = []
+  for link in get_transcript_links():
+    episodes.append(get_episode_from_link(link))
+  return episodes
+
+def get_episode_from_link(link):
+  print("fetching episode from link. . .")
+  html = html_from_url(link)
+  soup = BeautifulSoup(html, "html.parser")
+
+  #get body and title
+  title = soup.select('.page-header__page-subtitle > a:nth-child(1)')[0]['title']
+  body = ''
+  for ul in soup.select('#mw-content-text > ul'):
+    body += ul.prettify()
+  
+  # find and replace <b>, </b>, <i>, </i>, </li> with ''
+  artifacts = ['<b>', '</b>', '<i>', '</i>', '</li>', '</ul>', '<li>', '<ul>']
+  for art in artifacts:
+    body = body.replace(art, '')
+
+  return Episode(title, body)
+
+def get_transcript_links():
+  print("getting transcript links. . .")
+  wikiURL = "https://spongebob.fandom.com/wiki/List_of_transcripts"
+  transcriptSoup = BeautifulSoup(html_from_url(wikiURL), "html.parser")
+  transcriptLinkElements = transcriptSoup.find_all(is_transcript_link)
+  transcriptLinks = []
+
+  for elem in transcriptLinkElements:
+    transcriptLinks.append(make_link(elem))
+  return transcriptLinks
+
+def html_from_url(url):
+  fp = urllib.request.urlopen(url)
   mybytes = fp.read()
-  transcriptList = mybytes.decode("utf8")
   fp.close()
-
-  transcriptSoup = BeautifulSoup(transcriptList, "html.parser")
-  transcriptLinks = transcriptSoup.find_all(is_transcript_link)
-  print(transcriptLinks)
+  return mybytes.decode("utf8")
 
 def is_transcript_link(tag):
   isLink = tag.has_attr('href') and tag.has_attr('title')
@@ -27,5 +70,13 @@ def is_transcript_link(tag):
   
   headerId = tag.parent.parent.parent.parent.previous_sibling.previous_sibling.contents[0]['id']
   return re.compile('Season_(?!3)').search(headerId)
+
+def make_link(aTag):
+  return "https://spongebob.fandom.com" + aTag['href']
+
+class Episode:
+  def __init__(self, title, body):
+    self.title = title
+    self.body = body
 
 main()
